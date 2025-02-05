e37b18e2c73a7c9f86dde37f69de1a0622e08f6b
Michal Kašpárek <michal.kasparek@gmail.com>
2025-02-05 21:22:52 +0100

    opravé tagy na knihobotu

--- medojed.py
+++ medojed.py
@@ -3,16 +3,17 @@ import time
 import json
 import requests
 
+
 def medojed(vypis=True):
 
     def kelisova(odkaz):
-    
+
         r = requests.get(odkaz)
         je = None
         for kseft, dalsi_udaje in ksefty.items():
-            if kseft in odkaz.split('/')[2]:
-                je = dalsi_udaje['je']
-                neni = dalsi_udaje['neni']
+            if kseft in odkaz.split("/")[2]:
+                je = dalsi_udaje["je"]
+                neni = dalsi_udaje["neni"]
         if not je:
             print("Tento web neznám, přeskakuji.")
             return None
@@ -24,12 +25,14 @@ def medojed(vypis=True):
             return None
 
     try:
-        with open(os.path.join("config","odkazy.json"), 'r') as f:
+        with open(os.path.join("config", "odkazy.json"), "r") as f:
             seznam = json.loads(f.read())
-        with open(os.path.join("ksefty.json"), 'r') as f:
+        with open(os.path.join("ksefty.json"), "r") as f:
             ksefty = json.loads(f.read())
     except Exception as e:
-        print("Chybí některý z konfiguračních souborů. Viz následující chybové hlášení a soubor README.md.")
+        print(
+            "Chybí některý z konfiguračních souborů. Viz následující chybové hlášení a soubor README.md."
+        )
         print(e)
         quit()
 
@@ -50,8 +53,11 @@ def medojed(vypis=True):
 
     os.makedirs("data", exist_ok=True)
 
-    with open(os.path.join("data",f"{int(time.time())}.json"), 'w+', encoding="utf-8") as f:
+    with open(
+        os.path.join("data", f"{int(time.time())}.json"), "w+", encoding="utf-8"
+    ) as f:
         f.write(json.dumps(vysledky))
+
 
 if __name__ == "__main__":
     medojed()
